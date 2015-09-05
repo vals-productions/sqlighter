d1ac4fa2bc6aedf927d88d2585aa1c2616d8654b
vsayenko <vsayenko>
2015-09-05 16:09:45 -0700

    code adjustments

--- demo/crt-j2objc-link.sh
+++ demo/crt-j2objc-link.sh
@@ -5,7 +5,7 @@
 #
 
 # example
-# ln -s ../../m/j2objc-0.9.7 j2objc 
+ln -s ../../m/j2objc-0.9.7 j2objc 
 
-unlink j2objc
-ln -s <paht to j2objc> j2objc
+# unlink j2objc
+# ln -s <paht to j2objc> j2objc
